c793e083ebde9799e3ecdd6ce21b6163491d38e2
David Krauss <davey.krauss@googlemail.com>
2023-08-25 11:18:26 +0200

    add form and checkbox and submit button

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactzzzz
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Add new activity:</h1>
+      <form onSubmit="">
+        <label htmlFor="name">name of activity:</label>
+        <input name="name" id="name" />
+        <br></br>
+        <label htmlFor="goodweather">good weather activity:</label>
+        <input
+          name="goodweather"
+          id="goodweather"
+          type="checkbox"
+          value="good"
+        />
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
 
